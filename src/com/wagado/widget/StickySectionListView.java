b6f782d64778cd2cbca74f6d45f8b15b5836788b
Nikita Kleshchin <nfirex@gmail.com>
2012-10-15 13:42:56 +0400

    just Delete import

--- src/com/wagado/widget/StickySectionListView.java
+++ src/com/wagado/widget/StickySectionListView.java
@@ -23,7 +23,6 @@ import android.graphics.Canvas;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.util.SparseBooleanArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
